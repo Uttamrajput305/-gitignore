14440ea5819c17ca486b3b1097ae6458f850e0fb
Uttam Singh <uttamrajput305@gmail.com>
2022-12-06 15:41:30 +0530

    Update basics.js

--- basics.js
+++ basics.js
@@ -1,8 +1,18 @@
 const cowsay = require("cowsay");
 console.log(
 cowsay.say({
-text: "Piyush gandu",
+text: "hello world",
 e: "xx",
 T: "x ",
 })
 );
+
+var cowsay = require("cowsay");
+var Quote = require("inspirational -quotes")
+const quote = Quote.getRandomQuote()
+
+coonsole.llog(cowsay.say({
+  text: "quote",
+  e: "oO",
+  T: "U",
+}));
